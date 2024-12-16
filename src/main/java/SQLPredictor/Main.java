80630def6bc8232f6ddb02e72bc7e9d3cc6ae621
Dmitry358 <Dmitry358@github.com>
2024-12-16 12:34:41 +0100

    primo commit

---
+++ src/main/java/SQLPredictor/Main.java
@@ -0,0 +1,11 @@
+package SQLPredictor;
+
+public class Main{
+    public static void main(String[] args) {
+        String predictorWithFields = "<moto>(    t.aaa+77, t.tttt, t.km1 + left(t.nome1_1,11) + left(t.nome1_2,12) + left(t.nome1_3,13), t.bbb, tccc +44,  t.km2 +  left(t.nome2_1,21) +   left(t.nome2_2,22) , t.zzz  )";
+        //String predictorWithFields = "<       good   >(      qqq555eee + left(t.nome1_1,11) + left(t.nome1_2,12) )";
+        MLSQLExpander expander = new MLSQLExpander();
+        String query = expander.translate(predictorWithFields);
+        System.out.println(query);
+    }
+}
